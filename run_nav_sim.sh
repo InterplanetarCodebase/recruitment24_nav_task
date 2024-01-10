53a7ff1ec6fe32ababaa7a2957443f4bf6a12f08
Chinmoy Biswas <100117385+chinime@users.noreply.github.com>
2024-01-10 17:24:10 +0600

    Update run_nav_sim.sh
    Changes made:
    1. Completion tags added for debugging
    2. openv-python module was added in line 64

--- run_nav_sim.sh
+++ run_nav_sim.sh
@@ -9,13 +9,15 @@ if [ -d "task_ws" ]; then
   
   roslaunch recruitment24_nav_task simulation.launch &
 
+	echo "DONE --------------------------------- 1"
   source kivy_venv/bin/activate
   python3 src/recruitment24_nav_task/ui/controller.py &
   python3 src/recruitment24_nav_task/scripts/checker.py &
-  
   rosbag record -O nav_output.bag /odom /position_status &
   wait
   
+  echo "DONE --------------------------------- 2"
+
 else
   # Actions to perform if task_ws does not exist
   mkdir -p task_ws/src
@@ -26,24 +28,47 @@ else
   git \
   python3-pip \
   ros-noetic-turtlebot3 -y
+  
+  echo "DONE --------------------------------- 4"
+  
   git clone -b noetic-devel https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git
+  
+  echo "DONE --------------------------------- 5"
+  
   git clone https://github.com/InterplanetarCodebase/recruitment24_nav_task.git
+  
+  echo "DONE --------------------------------- 6"
+  
   cd ..
 
-  catkin_make
+  catkin_make 
+  
+  echo "DONE --------------------------------- 7"
+  
   source devel/setup.bash
   
   roslaunch recruitment24_nav_task simulation.launch &
 
+	echo "DONE --------------------------------- 8"
+
   python3 -m pip install --upgrade pip setuptools virtualenv
+  
+  echo "DONE --------------------------------- 9"
+  
   python3 -m venv kivy_venv
+  
+  echo "DONE --------------------------------- 10"
+  
   source kivy_venv/bin/activate
 
-  pip3 install "kivy[base]" rospkg defusedxml numpy pyyaml
+  pip3 install "kivy[base]" rospkg defusedxml numpy pyyaml opencv-python
+  
+  echo "DONE --------------------------------- 11"
 
   python3 src/recruitment24_nav_task/ui/controller.py &
   python3 src/recruitment24_nav_task/scripts/checker.py &
-  
   rosbag record -O nav_output.bag /odom /position_status &
   wait
+  
+  echo "DONE --------------------------------- 12"
 fi
